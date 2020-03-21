9444e3885d475b662f04fc1833ef965abed12ca4
kneurgao <kapil.neurgaonkar@gmail.com>
2020-03-22 00:43:08 +0530

    Applied reverse and length pipes in test app

--- packages/test/src/app/app.component.ts
+++ packages/test/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test';
+  title = 'test app is running!';
+  num = -12345.67890;
+  arr = ['a', 'b', 'c', 'd', 'ee'];
+  nul = null;
+  emptyArr = [];
+  obj = {a: 'b'};
+  date = new Date();
 }
