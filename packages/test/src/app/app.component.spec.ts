63c402e114f76bba13b407f6c888678489fc0712
kneurgao <kapil.neurgaonkar@gmail.com>
2020-04-11 16:40:45 +0530

    Corrections made in test cases

--- packages/test/src/app/app.component.spec.ts
+++ packages/test/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('test app is running!');
+    expect(compiled.querySelector('h2').textContent).toContain('test');
   });
 });
