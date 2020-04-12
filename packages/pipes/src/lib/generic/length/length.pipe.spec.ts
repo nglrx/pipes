fdada9ed078949b9621ed65ea9ac2d015a48b018
kneurgao <kapil.neurgaonkar@gmail.com>
2020-04-12 19:51:07 +0530

    Automated test cases of length and reverse pipes

--- packages/pipes/src/lib/generic/length/length.pipe.spec.ts
+++ packages/pipes/src/lib/generic/length/length.pipe.spec.ts
@@ -1,8 +1,30 @@
 import { LengthPipe } from './length.pipe';
 
 describe('LengthPipe', () => {
-  it('create an instance', () => {
-    const pipe = new LengthPipe();
+  let pipe: LengthPipe;
+
+  const str = 'test is running!';
+  const num = -12345.67890;
+  const arr = ['a', 'b', 'c', 'd', 'e'];
+
+  beforeEach(() => {
+    pipe = new LengthPipe();
+  });
+
+  it('should create an instance', () => {
     expect(pipe).toBeTruthy();
   });
+
+  it(`should find length of a string`, () => {
+    expect(pipe.transform(str)).toEqual(str.length);
+  });
+
+  it(`should find length of a number`, () => {
+    expect(pipe.transform(num)).toEqual(num.toString().length);
+  });
+
+  it(`should find length of an array`, () => {
+    expect(pipe.transform(arr)).toEqual(arr.length);
+  });
+
 });
--- packages/test/src/app/app.component.spec.ts
+++ packages/test/src/app/app.component.spec.ts
@@ -20,16 +20,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'test'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('test');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h2').textContent).toContain('test');
-  });
 });
