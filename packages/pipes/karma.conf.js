b84f3fa23d66e8aca44115b9665c1aafa070ad5b
Kapil Neurgaonkar <kapil.neurgaonkar@gmail.com>
2021-09-17 23:14:43 +0530

    Added support for testing on Edge Headless browser

--- packages/pipes/karma.conf.js
+++ packages/pipes/karma.conf.js
@@ -10,7 +10,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('@chiragrupani/karma-chromium-edge-launcher')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -25,10 +26,10 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessNoSandbox'],
+    browsers: ['EdgeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
+        base: 'EdgeHeadless',
         flags: ['--no-sandbox']
       }
     },
